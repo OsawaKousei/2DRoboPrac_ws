da9d75c2c44fb98d262332ba2a4b6b11f68b8a87
n622 <jwithelis@gmail.com>
2023-11-24 11:37:02 +0900

    起動には成功

--- ros2_ws/src/gazebo_world/launch/gazebo_world.launch.py
+++ ros2_ws/src/gazebo_world/launch/gazebo_world.launch.py
@@ -1,29 +1,23 @@
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
-    # Constants for paths to different files and folders
-    gazebo_pkg_name = 'gazebo_world'
-    world_file_path = 'worlds/display_stl.sdf'
+    world_file_path = PathJoinSubstitution([
+        get_package_share_directory('gazebo_world'),
+        'worlds',
+        'display_stl.sdf'
+    ])
 
-    gazebo_pkg_share = FindPackageShare(package=gazebo_pkg_name).find(gazebo_pkg_name)
-    world_path = os.path.join(gazebo_pkg_share, world_file_path)
-
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
             launch_arguments=[
                 ('gz_args', world_file_path)]
-    )
-
-    nodes = [
-        gazebo,
-    ]
-
-    return LaunchDescription(nodes)
+        )
+    ])
