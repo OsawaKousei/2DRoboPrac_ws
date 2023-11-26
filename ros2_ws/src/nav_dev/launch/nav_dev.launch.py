6b06db97023196c13e051cdd971e9e8f658a5c74
n622 <jwithelis@gmail.com>
2023-11-26 12:47:32 +0900

    addLidarModel

---
+++ ros2_ws/src/nav_dev/launch/nav_dev.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Configure ROS nodes for launch
+
+    # Setup project paths
+    pkg_project_gazebo = get_package_share_directory('nav_dev')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    # Setup to launch the simulator and Gazebo world
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': PathJoinSubstitution([
+            pkg_project_gazebo,
+            'worlds',
+            'nav_dev_world1.sdf'
+        ])}.items(),
+    )
+
+    return LaunchDescription([
+        gz_sim
+    ])
