4a7b7b5b8dacee1ec138a1141de0abd7fd7724a5
n622 <jwithelis@gmail.com>
2023-11-27 09:20:52 +0900

    save

--- ros2_ws/src/nav_dev/launch/nav_slam.launch.py
+++ ros2_ws/src/nav_dev/launch/nav_slam.launch.py
@@ -10,6 +10,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
+import xacro
 
 
 def generate_launch_description():
@@ -18,13 +19,20 @@ def generate_launch_description():
     # Setup project paths
     pkg_project_bringup = get_package_share_directory('nav_dev')
     pkg_project_gazebo = get_package_share_directory('nav_dev')
-    pkg_project_description = get_package_share_directory('nav_dev')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world_name = LaunchConfiguration('world_name', default='nav_dev_world')
+
+    launch_file_dir = os.path.join(get_package_share_directory('nav_dev'), 'launch')
+
     # Load the SDF file from "description" package
-    sdf_file  =  os.path.join(pkg_project_description, 'models', 'simpleDiff', 'model.sdf')
-    with open(sdf_file, 'r') as infp:
-        robot_desc = infp.read()
+    sdf = os.path.join(
+        get_package_share_directory('nav_dev'),
+        'models', 'simpleDiff', 'model.sdf')
+
+    doc = xacro.parse(open(sdf))
+    xacro.process_doc(doc)
 
     # Setup to launch the simulator and Gazebo world
     gz_sim = IncludeLaunchDescription(
@@ -37,17 +45,13 @@ def generate_launch_description():
         ])}.items(),
     )
 
-    # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='both',
-        parameters=[
-            {'use_sim_time': True},
-            {'robot_description': robot_desc},
-        ]
-    )
+    Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time,
+                         'robot_description': doc.toxml()}]),
 
     # Visualize in RViz
     rviz = Node(
@@ -65,21 +69,37 @@ def generate_launch_description():
             'config_file': os.path.join(pkg_project_bringup, 'config', 'teleop_bridge.yaml'),
             'qos_overrides./tf_static.publisher.durability': 'transient_local',
         }],
-        output='screen'
+        remappings=[
+            ("/odom/tf", "tf"),
+        ]
     )
+
+    map_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'odom'])
+
 
     teleop_node =  Node(
                 package='nav_dev',
                 executable='teleop_node',
-                output='screen',
                 prefix="xterm -e"
                 )
 
     return LaunchDescription([
         gz_sim,
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='If true, use simulated clock'),
+        DeclareLaunchArgument(
+            'world_name',
+            default_value=world_name,
+            description='World name'),
         DeclareLaunchArgument('rviz', default_value='true',description='Open RViz.'),
         bridge,
-        robot_state_publisher,
+        map_static_tf,
         rviz,
         teleop_node
     ])
