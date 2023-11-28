f5bff240338676e9eb084deb1fceeab1388a3921
n622 <jwithelis@gmail.com>
2023-11-28 21:03:09 +0900

    単独でのビルドに成功

--- ros2_ws/src/nav_dev/launch/nav_slam2.launch.py
+++ ros2_ws/src/nav_dev/launch/nav_slam2.launch.py
@@ -111,7 +111,45 @@ def generate_launch_description():
             output='both',
             parameters=[{'use_sim_time': use_sim_time,
                          'robot_description': doc.toxml()}])
+    
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('nav_dev'),
+            'maps',
+            'turtlebot3_world.yaml'))
 
+    param_file_name = 'waffle.yaml'
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('nav_dev'),
+            'params',
+            param_file_name))
+
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'rviz',
+        'nav2_default_view.rviz')
+    
+    nav2 = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir}.items(),
+        )
+    
+    rviz2 = Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen')
+    
     return LaunchDescription([
         ign_resource_path,
         ignition_spawn_entity,
@@ -133,8 +171,16 @@ def generate_launch_description():
 
         robot_state_publisher,
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([launch_file_dir, '/navigation2.launch.py']),
-            launch_arguments={'use_sim_time': use_sim_time}.items(),
-        ),
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        nav2,
+        rviz2,
     ])
