4276889ac01b611d9e96ef32a9368d310997b280
n622 <jwithelis@gmail.com>
2023-11-28 21:33:58 +0900

    bridgeのconfigファイルを使用

--- ros2_ws/src/nav_dev/launch/nav_slam2.launch.py
+++ ros2_ws/src/nav_dev/launch/nav_slam2.launch.py
@@ -67,27 +67,11 @@ def generate_launch_description():
     bridge = Node(
         package='ros_ign_bridge',
         executable='parameter_bridge',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[
-                # Velocity command (ROS2 -> IGN)
-                '/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
-                # Odometry (IGN -> ROS2)
-                '/odom@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
-                # TF (IGN -> ROS2)
-                '/odom/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
-                # Clock (IGN -> ROS2)
-                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
-                # Joint states (IGN -> ROS2)
-                '/joint_states@sensor_msgs/msg/JointState[ignition.msgs.Model',
-                # Lidar (IGN -> ROS2)
-                '/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
-                '/scan/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
-                # IMU (IGN -> ROS2)
-                '/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
-                # Camera (IGN -> ROS2)
-                '/camera/rgb/image_raw@sensor_msgs/msg/Image[ignition.msgs.Image',
-                '/camera/rgb/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
-                ],
+        parameters=[{
+            #brigdeのcofigファイルを指定
+            'config_file': os.path.join(pkg_share_dir, 'config', 'ros_gz_bridge.yaml'),
+            'qos_overrides./tf_static.publisher.durability': 'transient_local',
+        },{'use_sim_time': use_sim_time}],
         remappings=[
             ("/odom/tf", "tf"),
         ],
