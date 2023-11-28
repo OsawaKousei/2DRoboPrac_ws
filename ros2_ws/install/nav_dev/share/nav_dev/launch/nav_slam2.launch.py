c23212361a2ae49daf7d279ea44d9ede2e1fef0b
n622 <jwithelis@gmail.com>
2023-11-28 20:17:51 +0900

    bridge移動

--- ros2_ws/install/nav_dev/share/nav_dev/launch/nav_slam2.launch.py
+++ ros2_ws/install/nav_dev/share/nav_dev/launch/nav_slam2.launch.py
@@ -11,6 +11,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     world_name = LaunchConfiguration('world_name', default='turtlebot3_world')
     pkg_share_dir = get_package_share_directory('nav_dev')
+    model_path = os.path.join(pkg_share_dir, "models")
 
     launch_file_dir = os.path.join(pkg_share_dir, 'launch')
 
@@ -20,7 +21,7 @@ def generate_launch_description():
         name='IGN_GAZEBO_RESOURCE_PATH',value=[
         os.path.join("/opt/ros/humble", "share"),
         ":" +
-        os.path.join(pkg_share_dir, "models")])
+        model_path])
 
     # Spawn robot
     ignition_spawn_entity = Node(
@@ -58,6 +59,42 @@ def generate_launch_description():
             launch_arguments=[('ign_args', [' -r -v 3 ' +
                               world_only
                              ])])
+    
+    bridge = Node(
+        package='ros_ign_bridge',
+        executable='parameter_bridge',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[
+                # Velocity command (ROS2 -> IGN)
+                '/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
+                # Odometry (IGN -> ROS2)
+                '/odom@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
+                # TF (IGN -> ROS2)
+                '/odom/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
+                # Clock (IGN -> ROS2)
+                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+                # Joint states (IGN -> ROS2)
+                '/joint_states@sensor_msgs/msg/JointState[ignition.msgs.Model',
+                # Lidar (IGN -> ROS2)
+                '/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+                '/scan/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+                # IMU (IGN -> ROS2)
+                '/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
+                # Camera (IGN -> ROS2)
+                '/camera/rgb/image_raw@sensor_msgs/msg/Image[ignition.msgs.Image',
+                '/camera/rgb/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+                ],
+        remappings=[
+            ("/odom/tf", "tf"),
+        ],
+        output='screen'
+    )
+
+    map_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'odom'])
 
     return LaunchDescription([
         ign_resource_path,
@@ -75,10 +112,8 @@ def generate_launch_description():
             default_value=world_name,
             description='World name'),
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([launch_file_dir, '/ros_ign_bridge.launch.py']),
-            launch_arguments={'use_sim_time': use_sim_time}.items(),
-        ),
+        bridge,
+        map_static_tf,
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([launch_file_dir, '/robot_state_publisher.launch.py']),
