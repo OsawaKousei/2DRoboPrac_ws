58cfa96217827dbf53d303a7f0585625c36ca56b
n622 <jwithelis@gmail.com>
2023-11-30 04:53:13 +0900

    createLR5

--- ros2_ws/src/nav_dev/src/teleop_node.cpp
+++ ros2_ws/src/nav_dev/src/teleop_node.cpp
@@ -30,20 +30,20 @@ public:
             switch (msg->key)
             {
             case 's':
-                message.linear.x = -3;
+                message.linear.x = -1;
                 message.angular.z = 0;
                 break;
             case 'w':
-                message.linear.x = 5;
+                message.linear.x = 1;
                 message.angular.z = 0;
                 break;
             case 'a':
                 message.linear.x = 0;
-                message.angular.z = 2;
+                message.angular.z = 1;
                 break;
             case 'd':
                 message.linear.x = 0;
-                message.angular.z = -2;
+                message.angular.z = -1;
                 break;
             default:
                 message.linear.x = 0;
