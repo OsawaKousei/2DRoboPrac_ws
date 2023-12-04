048c58e9d0d45e427385207013e98f0342e85e9f
n622 <jwithelis@gmail.com>
2023-12-04 16:56:16 +0900

    lidar周期計測成功

--- ros2_ws/src/nav_real/src/counter_node.cpp
+++ ros2_ws/src/nav_real/src/counter_node.cpp
@@ -4,6 +4,7 @@
 #include "std_msgs/msg/string.hpp"
 #include "string"
 #include "iostream"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 using namespace std::chrono_literals;
 
@@ -27,20 +28,29 @@ public:
             this->publisher_->publish(message); 
         }; 
 
+
         timer_ = this->create_wall_timer(1000ms, publish_msg_callback);
 
         auto topic_callback = [this](const std_msgs::msg::String &msg) -> void {
             count ++;
         }; 
 
+        auto sensor_callback = [this](const sensor_msgs::msg::LaserScan &msg) -> void {
+                count ++;
+            };
+
         subscription_ = this->create_subscription<std_msgs::msg::String>
                 ("practice_topic", 10, topic_callback);
+
+        sensorsub_ = this->create_subscription<sensor_msgs::msg::LaserScan>
+                ("/scan", 10, sensor_callback);
     }
 private:
     // 上記の動作に必要なprivateメンバ
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sensorsub_;
 };
 
 int main(int argc, char *argv[]) {
