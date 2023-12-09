7f5de0c1178594f35539658a96e685cd7c5734eb
n622 <jwithelis@gmail.com>
2023-12-09 14:41:22 +0900

    custom_test_msgを削除

--- ros2_ws/src/custom_test_msgs/build/custom_test_msgs/CMakeFiles/custom_test_msgs_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for custom_test_msgs_uninstall.
-# This may be replaced when dependencies are built.
--- ros2_ws/src/custom_test_msgs/build/custom_test_msgs/custom_test_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/osawakousei/ros2_ws/src/custom_test_msgs/build/custom_test_msgs/custom_test_msgs__py
-# Build directory: /home/osawakousei/ros2_ws/src/custom_test_msgs/build/custom_test_msgs/custom_test_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
