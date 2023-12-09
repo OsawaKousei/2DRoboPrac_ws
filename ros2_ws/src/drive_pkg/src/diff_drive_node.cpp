f638ef5281fe11e733a393ef556a81bc7838c23a
n622 <jwithelis@gmail.com>
2023-12-09 18:17:58 +0900

    diff drive node 試作

--- ros2_ws/src/drive_pkg/src/diff_drive_node.cpp
+++ ros2_ws/src/drive_pkg/src/diff_drive_node.cpp
@@ -4,16 +4,26 @@
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "drive_pkg/msg/diff_drive.hpp"
+# include <bits/stdc++.h>
 
 using namespace std::chrono_literals;
 
 class DiffDriveNode : public rclcpp::Node {
+public:
+
+    //public変数を宣言
     std::string type_;
     std::double_t rad_;
     std::double_t dis_;
-public:
+
+    //cmd_velからモータの制御値を計算
+    void diffDrive(float cmd[2],float lx,float az){ //cmd[0]:右輪rps,cmd[1]:左輪rps,lx:m/s,az:rad/s
+        cmd[0] = (lx + dis_*az)/4*M_1_PI*rad_;
+        cmd[1] = (lx - dis_*az)/4*M_1_PI*rad_;
+    }
+
     DiffDriveNode() : Node("diff_drive_node") {
-        //使用するパラメータの宣言
+        //使用するパラメータの宣言(param名,初期値)、小数点を入れないとint型になるので注意
         declare_parameter("robot_type", "default");
         declare_parameter("wheel_radious", -1.0);
         declare_parameter("wheel_distance", -1.0);
@@ -22,25 +32,30 @@ public:
         type_ = get_parameter("robot_type").as_string();
         rad_ = get_parameter("wheel_radious").as_double();
         rad_ = get_parameter("wheel_distance").as_double();
+
+        //パラメータの確認
+        RCLCPP_INFO(this->get_logger(), "robot type:%s\r\n",type_.c_str());
+        RCLCPP_INFO(this->get_logger(), "wheel radious:%f\r\n",rad_);
+        RCLCPP_INFO(this->get_logger(), "wheel distance:%f\r\n",dis_);
         
+        //通信周りの記述
         publisher_ = this->create_publisher<drive_pkg::msg::DiffDrive>("cmd_motor", 10);
 
         auto topic_callback = [this](const geometry_msgs::msg::Twist &msg) -> void {
+
             auto message = drive_pkg::msg::DiffDrive();
-            
+            float ans[] = {0,0}; //ans[0]:右輪rps,ans[1]:左輪rps
+            diffDrive(ans, msg.linear.x, msg.angular.z); //(配列のポインタ渡し,x軸方向の速度m/s,z軸回りの角速度rad/s)
+
+            message.name = "differencial drive";
+            message.m1 = ans[0];
+            message.m2 = ans[1];
+
             this->publisher_->publish(message);
         }; 
 
         subscription_ = this->create_subscription<geometry_msgs::msg::Twist>
                 ("cmd_vel", 10, topic_callback);
-
-        auto publish_callback = [this]() -> void {
-            RCLCPP_INFO(this->get_logger(), "robot type:%s\r\n",type_.c_str());
-            RCLCPP_INFO(this->get_logger(), "wheel radious:%f\r\n",rad_);
-            RCLCPP_INFO(this->get_logger(), "wheel distance:%f\r\n",dis_);
-        }; 
-
-        timer_ = this->create_wall_timer(1000ms, publish_callback);
     }
 private:
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
