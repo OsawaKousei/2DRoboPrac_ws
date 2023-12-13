d80483241ffcb7f7cb077a2303b9ae833f982625
n622 <jwithelis@gmail.com>
2023-12-13 18:55:49 +0900

    copy diff drive

---
+++ ros2_ws/src/omni_control/src/omni_states_node.cpp
@@ -0,0 +1,249 @@
+#include <functional>
+#include <memory>
+#include <sstream>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "drive_msgs/msg/diff_drive.hpp"
+#include "drive_msgs/msg/diff_drive_enc.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "tf2_msgs/msg/tf_message.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "nav_msgs/msg/odometry.hpp"
+#include <cmath>
+#include "rclcpp/qos.hpp"
+
+using namespace std::chrono_literals;
+
+class StatesPubNode : public rclcpp::Node {
+public:
+
+    //ロボットの種類
+    std::string type_;
+    //右輪と左輪の半径
+    std::double_t rad_;
+    //右輪と左輪の距離
+    std::double_t dis_;
+    //lxエンコーダの半径
+    std::double_t enc1;
+    //azエンコーダの半径
+    std::double_t enc2;
+    //ロボットの中心からazエンコーダまでの距離
+    std::double_t enc2dis;
+
+    //lxエンコーダの回転数
+    float lx = 0;
+    //azエンコーダの回転数
+    float az = 0;
+    //lxエンコーダの回転数の差分
+    float lx_d = 0;
+    //ロボットのx座標
+    float x = 0;
+    //ロボットのy座標
+    float y = 0;
+    //ロボットの向き(rad)
+    float th = 0;
+
+    //ロボットの姿勢を計算
+    void calcPose(){
+        //azはエンコーダーの回転数、enc2はエンコーダーの半径、enc2disはロボットの中心からエンコーダーまでの距離
+        //azをラジアンに変換してロボットの姿勢を計算
+        th = az*2*M_PI*enc2/(enc2dis);
+
+        //lx_dはlxエンコーダの回転数の差分、enc1はlxエンコーダの半径
+        //lx_dとenc1からロボットの移動距離を計算
+        float x_d = lx_d * 2 * M_PI * enc1;
+
+        //xはロボットのx座標、yはロボットのy座標
+        //thはロボットの向き
+        //x,yからthの向きにx_dだけ進んだ座標を計算
+        x = x + x_d * cos(th);
+        y = y + x_d * sin(th);
+    }
+
+    
+    StatesPubNode() : Node("states_pub_node") {
+        //パラメータの宣言
+        declare_parameter("robot_type", "default");
+        declare_parameter("wheel_radious", -1.0);
+        declare_parameter("wheel_distance", -1.0);
+        declare_parameter("lxenc_radious", -1.0);
+        declare_parameter("azenc_radious", -1.0);
+        declare_parameter("azenc_distance", -1.0);
+
+        //パラメータの取得
+        type_ = get_parameter("robot_type").as_string();
+        rad_ = get_parameter("wheel_radious").as_double();
+        dis_ = get_parameter("wheel_distance").as_double();
+        enc1 = get_parameter("lxenc_radious").as_double();
+        enc2 = get_parameter("azenc_radious").as_double();
+        enc2dis = get_parameter("azenc_distance").as_double();
+
+        //パラメータの表示
+        RCLCPP_INFO(this->get_logger(), "robot type:%s\r\n",type_.c_str());
+        RCLCPP_INFO(this->get_logger(), "wheel radious:%f\r\n",rad_);
+        RCLCPP_INFO(this->get_logger(), "wheel distance:%f\r\n",dis_);
+        RCLCPP_INFO(this->get_logger(), "lxenc_radious:%f\r\n",enc1);
+        RCLCPP_INFO(this->get_logger(), "azenc_radious:%f\r\n",enc2);
+        RCLCPP_INFO(this->get_logger(), "azenc_distnace:%f\r\n",enc2dis);
+
+        //publisherの作成
+        jointpub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+        odompub_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+        //tfをpublishするためのtf_broadcasterを作成
+        tf_broadcaster_ =std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+        //joint statesをpublish
+        auto joint_publisher = [this](float m1enc, float m2enc) -> void {
+            //joint statesを作成
+            auto joint = sensor_msgs::msg::JointState();
+
+            //joint statesの値を設定
+            joint.header.stamp = this->get_clock()->now();
+            joint.name.resize(6);
+            joint.name[0] = "base_joint";
+            joint.name[1] = "lidar_joint";
+            joint.name[2] = "wheel_left_joint";
+            joint.name[3] = "wheel_right_joint";
+            joint.name[4] = "caster_back_right_joint";
+            joint.name[5] = "caster_back_left_joint";
+            joint.position.resize(6);
+            joint.position[0] = 0.0;
+            joint.position[1] = 0.0;
+            //m1encは右輪エンコーダーの回転数、m2encは左輪エンコーダの回転数
+            //rad_は右輪と左輪の半径
+            //m1encとm2encから右輪と左輪の角度を計算
+            joint.position[2] = m1enc*2*M_PI*rad_;
+            joint.position[3] = m2enc*2*M_PI*rad_;
+            joint.position[4] = 0.0;
+            joint.position[5] = 0.0;
+
+            //joint statesをpublish
+            this->jointpub_->publish(joint);
+        };
+
+        //odomをpublish
+        auto odom_publisher = [this]() -> void {
+            auto odom = nav_msgs::msg::Odometry();
+
+            odom.header.stamp = this->get_clock()->now();
+            odom.header.frame_id = "/odom";
+
+            odom.child_frame_id = "base_footprint";
+
+            odom.pose.pose.position.x = x;
+            odom.pose.pose.position.y = y;
+            odom.pose.pose.position.z = 0;
+
+            //ロボットの向きをクォータニオンで表現
+            tf2::Quaternion r;
+            r.setRPY(0, 0, th);
+
+            //クォータニオンをodomに設定
+            odom.pose.pose.orientation.x = r.x();
+            odom.pose.pose.orientation.y = r.y();
+            odom.pose.pose.orientation.z = r.z();
+            odom.pose.pose.orientation.w = r.w();
+
+            //twistを設定
+            odom.twist.twist.linear.x = 0;
+            odom.twist.twist.linear.y = 0;
+            odom.twist.twist.linear.z = 0;
+            odom.twist.twist.angular.x = 0;
+            odom.twist.twist.angular.y = 0;
+            odom.twist.twist.angular.z = 0;
+
+            //odomをpublish
+            this->odompub_->publish(odom);
+        };
+
+        auto tf_publisher = [this]() -> void {
+            auto tf = geometry_msgs::msg::TransformStamped();
+
+            tf.header.stamp = this->get_clock()->now();
+            tf.header.frame_id = "odom";
+            tf.child_frame_id = "base_footprint";
+
+            tf.transform.translation.x = x;
+            tf.transform.translation.y = y;
+            tf.transform.translation.z = 0;
+
+            tf2::Quaternion q;
+            q.setRPY(0, 0, th);
+
+            tf.transform.rotation.x = q.x();
+            tf.transform.rotation.y = q.y();
+            tf.transform.rotation.z = q.z();
+            tf.transform.rotation.w = q.w();
+
+            //<geometry_msgs::msg::TransformStamped>をtfに変換してpublish
+            tf_broadcaster_->sendTransform(tf);
+
+            auto maptf = geometry_msgs::msg::TransformStamped();
+
+            maptf.header.stamp = this->get_clock()->now();
+            maptf.header.frame_id = "map";
+            maptf.child_frame_id = "odom";
+
+            maptf.transform.translation.x = 0;
+            maptf.transform.translation.y = 0;
+            maptf.transform.translation.z = 0;
+
+            tf2::Quaternion p;
+            p.setRPY(0, 0, 0);
+
+            maptf.transform.rotation.x = p.x();
+            maptf.transform.rotation.y = p.y();
+            maptf.transform.rotation.z = p.z();
+            maptf.transform.rotation.w = p.w();
+
+            tf_broadcaster_->sendTransform(maptf);
+        };
+
+        //subscriptionのコールバック関数
+        auto sub_callback = [this, joint_publisher]
+            (const drive_msgs::msg::DiffDriveEnc &msg) -> void {
+            //joint statesをpublish
+            //msg.m1encは右輪エンコーダーの回転数、msg.m2encは左輪エンコーダの回転数
+            joint_publisher(msg.m1enc,msg.m2enc);
+
+            //lxエンコーダの回転数の差分を計算
+            lx_d = msg.lxenc - lx;
+            //エンコーダの値を更新
+            lx = msg.lxenc;
+            az = msg.azenc;
+        }; 
+        //subscriptionを作成
+        subscription_ = this->create_subscription<drive_msgs::msg::DiffDriveEnc>("enc_val",10,sub_callback);
+        
+        //timerのコールバック関数
+        auto pub_callback = [this, odom_publisher,tf_publisher]() -> void {
+            //ロボットの姿勢を計算
+            calcPose();
+            
+            //odomをpublish
+            odom_publisher();
+            //tfをpublish
+            tf_publisher();
+        }; 
+        
+        //timerを作成
+        timer_ = this->create_wall_timer(2ms, pub_callback);
+    }
+private:
+    rclcpp::Subscription<drive_msgs::msg::DiffDriveEnc>::SharedPtr subscription_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr jointpub_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odompub_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    rclcpp::TimerBase::SharedPtr timer_; 
+};
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<StatesPubNode>());
+    rclcpp::shutdown();
+    return 0;
+}
